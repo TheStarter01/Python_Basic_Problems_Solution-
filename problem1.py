05205a5059a8f889e93930064c3c4932011f74de
Akhil khandelwal <99923897+TheStarter01@users.noreply.github.com>
2022-10-05 13:54:53 +0530

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,10 @@
+# problem 1
+num1 = int(input("Enter the First Number1 : ")) 
+num2 = int(input("Enter the Second Number2 : "))
+if(num1<=31 and num2<=31):
+    Multiplie = num1 * num2 
+    print(Multiplie)
+elif(num1>=31 and num2>=31):
+    Add =  num1 + num2
+    print(Add)
+else:print("Please Enter the valid number")
